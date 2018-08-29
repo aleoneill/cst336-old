f4e73af397c28f774eb00f4460c64a029e84fb7d
Alexis Oneill <aoneill@csumb.edu>
2018-08-29 22:05:13 +0000

    Lab 1 files - not completed

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <title>
+        </title>
+    </head>
+    <body>
+    <?php
+    // A simple web site in Cloud9 that runs through Apache
+    // Press the 'Run' button on the top to start the web server,
+    // then click the URL that is emitted to the Output tab of the console
 
-echo "<h1> Alexis (Lexi) O'Neill's CST 336 site <h1>" ;
+    echo "<h1> Alexis (Lexi) O'Neill's CST 336 site <h1>" ;
 
-?>
-</body>
+    ?>
+    </body>
 </html>
